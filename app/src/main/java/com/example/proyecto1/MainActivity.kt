8647954ea65755bbbdf348a51fdc34a4d3b92bac
Alejandro Garcia <15234404@modelo.edu.mx>
2026-03-02 09:47:10 -0600

    add MainActivity as default screen when running app
    linked Activity3.kt to "Juagar" button and OpcionesActivity.kt to "Opciones" button

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.proyecto1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, Activity3::class.java))
-        finish()
+        setContentView(R.layout.activity_main)
+
+        val jugarButton: Button = findViewById(R.id.jugar_button)
+        jugarButton.setOnClickListener {
+            val intent = Intent(this, Activity3::class.java)
+            startActivity(intent)
+        }
+
+        val opcionesButton: Button = findViewById(R.id.opciones_button)
+        opcionesButton.setOnClickListener {
+            val intent = Intent(this, OpcionesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
